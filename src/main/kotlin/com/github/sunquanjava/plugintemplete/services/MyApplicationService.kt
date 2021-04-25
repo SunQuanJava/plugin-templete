82ea7654ab3a56d33bdca6b59129e51e079eb33d
GitHub Action <action@github.com>
2021-04-25 07:07:17 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/sunquanjava/plugintemplete/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sunquanjava.plugintemplete.services
+
+import com.github.sunquanjava.plugintemplete.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
